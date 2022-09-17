ac557c49db79cce2931b0aa0a1fc8d485f6b0a29
hgm <HeribertoGM1999@outlook.com>
2022-09-16 20:31:44 -0500

    added clipboard button and include eval on response

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
-function convert() {
-	const input = document.getElementById("text-input");
-	const output = document.getElementById("text-output");
+const input = document.getElementById("text-input");
+const output = document.getElementById("text-output");
 
+function convert() {
 	const text = input.value;
-	output.value = EncodeString(text);
+	output.value = `eval(${EncodeString(text)});`;
+}
+
+function copy() {
+	if (navigator.clipboard) {
+		navigator.clipboard.writeText(output.value).then(
+			() => {
+				alert("Copied to Clipboard!");
+			},
+			(err) => {
+				alert("Failed to copy to clipboard");
+				console.log(err);
+			}
+		);
+	} else if (window.clipboardData) {
+		window.clipboardData.setData("Text", input);
+	}
 }
